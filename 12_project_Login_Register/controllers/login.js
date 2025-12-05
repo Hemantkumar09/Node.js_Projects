421266b4b5ec43e7623791c0fc9f2957fc22992c
Hemant Kumar <hemant9451.k@gmail.com>
2025-12-05 09:37:18 +0530

    code cleanup

--- 12_project_Login_Register/controllers/login.js
+++ 12_project_Login_Register/controllers/login.js
@@ -1,19 +1,6 @@
 import { UserInfo } from "../Model/UserInfo.js";
-// import { v2 as cloudinary } from 'cloudinary';
-
-
-// cloudinary.config({ 
-//     cloud_name: 'dhuaumafl', 
-//     api_key: '617585827482663', 
-//     api_secret: 'FwA-gvvsswjj7IFdDr6msMEx1XI' 
-// });
 
 export const UserLogin = async (req, res) => {
-    // const file = req.file.path
-    // const cloudinaryRes = await cloudinary.uploader.upload(file,{
-    //     folder:"Node.js_Project_User_Info"
-    // });
-    // console.log("response:- ", cloudinaryRes);
     const {email, password} = req.body;
 
     const user = await UserInfo.findOne({email});
@@ -24,6 +11,5 @@ export const UserLogin = async (req, res) => {
     }else{
         res.render("Profile.ejs", {user});
     }
-    //res.redirect('/login-page');
     
 }
